59892249038516e7dc6a680d2cbfaa080af21f61
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-04-26 15:07:40 +0545

    Working LOL

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
-
 const darkModeButton = document.getElementById('dark-mode-btn');
 const darkModeIcon = document.getElementById('dark-mode-icon');
+const dateElement = document.querySelector('.date');
+const fineGoldTolaElement = document.querySelector('.fine-gold .tola');
+const fineGoldGramElement = document.querySelector('.fine-gold .gram');
+const tejabiGoldTolaElement = document.querySelector('.tejabi-gold .tola');
+const tejabiGoldGramElement = document.querySelector('.tejabi-gold .gram');
+const silverTolaElement = document.querySelector('.silver .tola');
+const silverGramElement = document.querySelector('.silver .gram');
+const dayElement = document.querySelector('.day');
+const monthElement = document.querySelector('.month');
+const yearElement = document.querySelector('.year');
 
 function toggleDarkModeIcon() {
     if (darkModeIcon.getAttribute('src') === 'images/moon.png') {
@@ -14,3 +23,19 @@ darkModeButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     toggleDarkModeIcon();
 });
+fetch('Values.json')
+    .then(response => response.json())
+    .then(data => {
+        const { day, month, year, fine_gold_tola, fine_gold_gram, tejabi_gold_tola, tejabi_gold_gram, silver_tola, silver_gram } = data[0];
+        dateElement.textContent = `Gold price on ${day} ${month} ${year}`;
+        fineGoldTolaElement.textContent = `Tola: ${fine_gold_tola}`;
+        fineGoldGramElement.textContent = `Gram: ${fine_gold_gram}`;
+        tejabiGoldTolaElement.textContent = `Tola: ${tejabi_gold_tola}`;
+        tejabiGoldGramElement.textContent = `Gram: ${tejabi_gold_gram}`;
+        silverTolaElement.textContent = `Tola: ${silver_tola}`;
+        silverGramElement.textContent = `Gram: ${silver_gram}`;
+        dayElement.textContent = day;
+        monthElement.textContent = month;
+        yearElement.textContent = year;
+    })
+    .catch(error => console.error(error));
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -7,14 +7,20 @@ class GoldSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        day = response.css('div.rate-date-day::text').get()
+        month = response.css('div.rate-date-month::text').get()
+        year = response.css('div.rate-date-year::text').get()
         fine_gold_gram = response.css('div.rate-gold p:contains("FINE GOLD (9999)") b::text').get()
         tejabi_gold_gram = response.css('div.rate-gold p:contains("TEJABI GOLD") b::text').get()
         silver_gram = response.css('div.rate-silver p:contains("SILVER") b::text').get()
-        fine_gold_tola = response.css('div.rate-gold p:contains("FINE GOLD (9999)") b::text').get()
-        tejabi_gold_tola = response.css('div.rate-gold p:contains("TEJABI GOLD") b::text').get()
-        silver_tola = response.css('div.rate-silver p:contains("SILVER") b::text').get()
+        fine_gold_tola = response.css('div#vtab div:contains("FINE GOLD (9999)") + div div.rate-gold p:first-child b::text').get()
+        tejabi_gold_tola = response.css('div#vtab div:contains("TEJABI GOLD") + div div.rate-gold p:first-child b::text').get()
+        silver_tola = response.css('div#vtab div:contains("SILVER") + div div.rate-silver p:first-child b::text').get()
 
         yield {
+            'day': day,
+            'month': month,
+            'year': year,
             'fine_gold_gram': fine_gold_gram,
             'tejabi_gold_gram': tejabi_gold_gram,
             'silver_gram': silver_gram,
