b02d395f19cd772cca60496e9c815f1adcfef9b0
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-04-29 12:13:34 +0545

    Update

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -14,7 +14,7 @@ class GoldSpider(scrapy.Spider):
         tejabi_gold_gram = response.css('div.rate-gold p:contains("TEJABI GOLD") b::text').get()
         silver_gram = response.css('div.rate-silver p:contains("SILVER") b::text').get()
         fine_gold_tola = response.css('div#vtab div:contains("FINE GOLD (9999)") + div div.rate-gold p:first-child b::text').get()
-        tejabi_gold_tola = response.css('div#vtab div:contains("TEJABI GOLD") + div div.rate-gold p:first-child b::text').get()
+        tejabi_gold_tola = response.css('div#vtab div:contains("TEJABI GOLD") + div div.rate-gold:nth-child(2) p:first-child b::text').get()
         silver_tola = response.css('div#vtab div:contains("SILVER") + div div.rate-silver p:first-child b::text').get()
 
         yield {
